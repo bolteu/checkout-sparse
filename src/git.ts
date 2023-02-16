8fa0048ec097c05c31a9e1c478849dd2ea871898
usetheplatform <anatoliy.belobrovik@bolt.eu>
2023-02-16 12:13:30 +0200

    Support refs/heads/ and refs/tags/ as an input

---
+++ src/git.ts
@@ -0,0 +1,52 @@
+import * as exec from "@actions/exec";
+import * as github from "@actions/github";
+
+export class Git {
+  repository: string;
+  octokit: ReturnType<typeof github.getOctokit>;
+
+  constructor(
+    repository: string,
+    octokit: ReturnType<typeof github.getOctokit>
+  ) {
+    this.repository = repository;
+    this.octokit = octokit;
+  }
+
+  async branchExists(pattern: string): Promise<boolean> {
+    try {
+      const response = await this.octokit.rest.git.listMatchingRefs({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        ref: `heads/${pattern}`,
+      });
+
+      return !!response.data.length;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async tagExists(pattern: string): Promise<boolean> {
+    try {
+      const response = await this.octokit.rest.git.listMatchingRefs({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        ref: `tags/${pattern}`,
+      });
+
+      return !!response.data.length;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async checkoutSparse(ref: string, files: string): Promise<void> {
+    await exec.exec(
+      `git clone --filter=blob:none --no-checkout --depth 1 --sparse ${this.repository} . --branch ${ref}`
+    );
+    await exec.exec(`git sparse-checkout init --cone`);
+    await exec.exec(`git sparse-checkout add ${files}`);
+    await exec.exec(`git checkout`);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,82 @@
-import * as core from "@actions/core"
-import * as exec from "@actions/exec"
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Git } from "./git";
 
-async function run() {
-    try {
-        let files = core.getInput("files", { required: true })
-        let ref = core.getInput("ref", { required: true })
-        let token = core.getInput("github-token") || process.env.GITHUB_TOKEN
-        let repository = `https://${process.env.GITHUB_ACTOR}:${token}@github.com/${process.env.GITHUB_REPOSITORY}.git`
+function isSHA(input?: string) {
+  if (!input) {
+    return false;
+  }
 
-        await exec.exec(`git clone --filter=blob:none --no-checkout --depth 1 --sparse ${repository} . --branch ${ref}`)
-        await exec.exec(`git sparse-checkout init --cone`)
-        await exec.exec(`git sparse-checkout add ${files}`)
-        await exec.exec(`git checkout`)
-    } catch (error) {
-        core.setFailed(error)
-    }
+  // Git SHA-1s are 40-character hexadecimal strings
+  if (input.length !== 40) {
+    return false;
+  }
+
+  if (!/^[0-9a-fA-F]+$/.test(input)) {
+    return false;
+  }
+
+  return true;
 }
 
-run()
+async function parseRef(git: Git, ref: string): Promise<string> {
+  // sha
+  if (isSHA(ref)) {
+    throw new Error(
+      `A git SHA ${ref} is not supported. Please provide a branch or a tag.`
+    );
+  }
+
+  // refs/heads/
+  if (ref.startsWith("refs/heads/")) {
+    const branch = ref.substring("refs/heads/".length);
+    return branch;
+  }
+
+  // refs/tags/
+  else if (ref.startsWith("refs/tags/")) {
+    const tag = ref.substring("refs/tags/".length);
+    return tag;
+  }
+
+  // refs/pull
+  else if (ref.startsWith("refs/pull/")) {
+    throw new Error(
+      `A reference to a pull request ${ref} is not supported. Please provide a branch or a tag.`
+    );
+  }
+
+  // Unqualified ref, check for a matching branch or tag
+  else {
+    if (await git.branchExists(ref)) {
+      return ref;
+    } else if (await git.tagExists(ref)) {
+      return `refs/tags/${ref}`;
+    } else {
+      throw new Error(
+        `A branch or tag with the name '${ref}' could not be found.`
+      );
+    }
+  }
+}
+
+async function run() {
+  try {
+    let files = core.getInput("files", { required: true });
+    let ref = core.getInput("ref", { required: true });
+    let token = core.getInput("github-token") || process.env.GITHUB_TOKEN;
+    let repository = `https://${process.env.GITHUB_ACTOR}:${token}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+    let octokit = github.getOctokit(token);
+
+    let git = new Git(repository, octokit);
+
+    const branch_or_tag = await parseRef(git, ref);
+
+    await git.checkoutSparse(branch_or_tag, files);
+  } catch (error) {
+    core.setFailed(error);
+    console.error(error);
+  }
+}
+
+run();
